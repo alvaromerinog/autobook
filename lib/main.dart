3a51deea9d45c9590713f328e6c6542da625b0b6
Alvaro Merino <amerino.informatica@gmail.com>
2021-06-11 20:04:42 +0200

    Add missing configurations and pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:autobook/pages/editMaintenancePage.dart';
 import 'package:autobook/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:autobook/pages/loading.dart';
@@ -6,6 +7,9 @@ import 'package:autobook/pages/confirmEmail.dart';
 import 'package:autobook/pages/forgot.dart';
 import 'package:autobook/pages/confirmReset.dart';
 import 'package:autobook/pages/changePwd.dart';
+import 'package:autobook/pages/newVehiclePage.dart';
+import 'package:autobook/pages/editVehiclePage.dart';
+import 'package:autobook/pages/newMaintenancePage.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -18,6 +22,10 @@ void main() {
     '/confirmEmail': (context) => ConfirmEmail(),
     '/home': (context) => Home(),
     '/changepsw': (context) => ChangePwd(),
+    '/newVehicle': (context) => NewVehiclePage(),
+    '/editVehicle': (context) => EditVehiclePage(),
+    '/newMaintenance': (context) => NewMaintenancePage(),
+    '/editMaintenance': (context) => EditMaintenancePage(),
   },
   ));
 }
