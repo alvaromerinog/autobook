cc8da272b2e759223bb85b4e7000aa3a130e058f
Alvaro Merino <amerino.informatica@gmail.com>
2021-06-11 19:40:35 +0200

    Implement initial vehicles functions

---
+++ lib/utils/selectVehicles.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_api/amplify_api.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class Vehicles {
+  AuthUser user;
+  String email;
+/*
+  Future<RestResponse> getVehicles() async {
+      List<int> bodyDigits = '{\"mail":\"$email\"}'.codeUnits;
+      Uint8List body = Uint8List.fromList(bodyDigits);
+      Map<String, String> headers = {'Authorization': 'Bearer ${user.userId}'};
+      RestOptions restOptions = RestOptions(path: '/vehicles', body: body, headers: headers);
+      RestOperation operation = AmplifyAPI.post(restOptions: restOptions);
+      Future<RestResponse> response = (await operation.response) as Future<RestResponse>;
+      return response;
+    }
+    */
+
+  dynamic getVehicles(email) async{
+    final response = await post(
+      Uri.parse('https://a06zlp66q2.execute-api.eu-west-1.amazonaws.com/dev/vehicles'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(<String, String>{
+        'mail': email,
+      }),
+    );
+    if(response.statusCode == 200){
+      dynamic body = jsonDecode(response.body);
+      return body;
+    } else {
+      return null;
+    }
+  }
+}
