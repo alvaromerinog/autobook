71206e77736d875479cf0df8329a03c566d7f544
Alvaro Merino <amerino.informatica@gmail.com>
2021-08-03 21:41:23 +0200

    Fix reload pages when navigating and add refresh page

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -6,14 +6,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class History extends StatefulWidget {
+  final String email;
+  History({required this.email});
+
   @override
-  _HistoryState createState() => _HistoryState();
+  _HistoryState createState() => _HistoryState(email: this.email);
 }
 
 class _HistoryState extends State<History> {
-  String? email;
+  String email;
+  _HistoryState({required this.email});
+
   List? maintenances;
-  String registration = Vehicles.getRegistration();
+  String registration = '';
   Map? arguments;
   Widget maintenancesWidget = SpinKitChasingDots(
     color: Colors.blue[800],
@@ -27,7 +32,22 @@ class _HistoryState extends State<History> {
     }
   }
 
-  void editMaintenance(registration, idMaintenance, description, dateMaintenance, odometer) async {
+  @override
+  void didUpdateWidget(covariant History oldWidget) {
+    String newRegistration = Vehicles.getRegistration();
+    if (newRegistration != registration) {
+      registration = newRegistration;
+      maintenancesWidget = SpinKitChasingDots(
+        color: Colors.blue[800],
+        size: 50.0,
+      );
+      getMaintenances();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void editMaintenance(registration, idMaintenance, description,
+      dateMaintenance, odometer) async {
     Map arguments = {
       'email': email,
       'registration': registration,
@@ -39,73 +59,98 @@ class _HistoryState extends State<History> {
     Navigator.pushNamed(context, '/editMaintenance', arguments: arguments);
   }
 
-  void getEmail() async {
-    getMaintenances();
+  void getMaintenances() async {
+    maintenances = await MaintenancesSelect(
+            email: this.email, registration: this.registration)
+        .getMaintenances();
+    this.buildMaintenances(maintenances);
   }
 
-  void getMaintenances() async {
-    dynamic response =
-        await MaintenancesSelect(email: email, registration: registration).getMaintenances();
-    if (response != null && !response['result'].isEmpty) {
-      maintenances = response['result'];
+  void buildMaintenances(List? vehicles) {
+    if (maintenances!.length != 0) {
       setState(() {
         maintenancesWidget = ListView.builder(
+          physics: const AlwaysScrollableScrollPhysics(),
           itemCount: maintenances!.length,
           itemBuilder: (BuildContext context, int index) {
             return new Card(
-                elevation: 20,
-                child: ListTile(
-                    tileColor: Colors.blue,
-                    title: Text(
-                        '${maintenances![index]['description']}\n${maintenances![index]['date_maintenance']}'),
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      child: maintenances![index]['description'].substring(0,6) == 'CAMBIO' ? Icon(
-                        Icons.sync_rounded,
-                        color: Colors.purple[700],
-                      ) : Icon(
-                        Icons.handyman_rounded,
-                        color: Colors.orange[700],
+              elevation: 20,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(7.0)),
+              ),
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                ),
+                tileColor: Colors.white70,
+                title: Text(
+                    '${maintenances![index]['description']}\n${maintenances![index]['date_maintenance']}'),
+                leading: maintenances![index]['description'].substring(0, 6) ==
+                        'CAMBIO'
+                    ? CircleAvatar(
+                        backgroundColor: Colors.amber,
+                        child: Icon(
+                          Icons.sync_problem_rounded,
+                          color: Colors.white,
+                        ),
+                      )
+                    : CircleAvatar(
+                        backgroundColor: Colors.red,
+                        child: Icon(
+                          Icons.handyman_rounded,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    trailing: RawMaterialButton(
-                      onPressed: () {
-                        String registration =
-                            maintenances![index]['registration'];
-                        String idMaintenance =
-                            maintenances![index]['id_maintenance'];
-                        String description = maintenances![index]['description'];
-                        String dateMaintenance =
-                            maintenances![index]['date_maintenance'];
-                        String odometer = maintenances![index]['odometer'];
-                        editMaintenance(registration, idMaintenance,
-                            description, dateMaintenance, odometer);
-                      },
-                      elevation: 2.0,
-                      fillColor: Colors.white,
-                      child: Icon(
-                        Icons.edit,
-                        color: Colors.blue[800],
-                      ),
-                      padding: EdgeInsets.all(10.0),
-                      shape: CircleBorder(),
-                    )));
+                trailing: RawMaterialButton(
+                  onPressed: () {
+                    String registration = maintenances![index]['registration'];
+                    String idMaintenance =
+                        maintenances![index]['id_maintenance'];
+                    String description = maintenances![index]['description'];
+                    String dateMaintenance =
+                        maintenances![index]['date_maintenance'];
+                    String odometer = maintenances![index]['odometer'];
+                    editMaintenance(registration, idMaintenance, description,
+                        dateMaintenance, odometer);
+                  },
+                  elevation: 2.0,
+                  fillColor: Colors.blue[800],
+                  child: Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                  ),
+                  padding: EdgeInsets.all(10.0),
+                  shape: CircleBorder(),
+                ),
+              ),
+            );
           },
         );
       });
     } else {
       setState(() {
-        maintenancesWidget = Card(
-          elevation: 20,
-          child: ListTile(
-            leading: Icon(
-              Icons.sentiment_very_dissatisfied,
-              color: Colors.blue[800],
-              size: 30,
+        maintenancesWidget = ListView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(7.0)),
+              ),
+              elevation: 20,
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                ),
+                leading: Icon(
+                  Icons.bubble_chart_rounded,
+                  color: Colors.blue[800],
+                  size: 30,
+                ),
+                title: Text('No se han encontrado mantenimientos',
+                    style: TextStyle(fontSize: 20.0)),
+              ),
             ),
-            title: Text('No se han encontrado mantenimientos',
-                style: TextStyle(fontSize: 20.0)),
-          ),
+          ],
         );
       });
     }
@@ -114,20 +159,30 @@ class _HistoryState extends State<History> {
   @override
   void initState() {
     super.initState();
-    getEmail();
+    getMaintenances();
+  }
+
+  Future<void> _refreshData() async {
+    this.getMaintenances();
+    await Future.delayed(Duration(seconds: 3));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
+        heroTag: "btn2",
         child: Icon(Icons.add),
         onPressed: () {
           Navigator.pushNamed(context, '/newMaintenance', arguments: arguments);
         },
       ),
-      body: Container(
-        child: maintenancesWidget,
+      body: SafeArea(
+        child: RefreshIndicator(
+          child: maintenancesWidget,
+          onRefresh: _refreshData,
+          displacement: 40.0,
+        ),
       ),
     );
   }
--- lib/pages/vehicles.dart
+++ lib/pages/vehicles.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Vehicles extends StatefulWidget {
+  final String email;
+  Vehicles({required this.email});
+
   @override
-  _VehiclesState createState() => _VehiclesState();
+  _VehiclesState createState() => _VehiclesState(email: email);
 
   static String getRegistration() {
     return _VehiclesState.selectedRegistration;
@@ -12,7 +15,9 @@ class Vehicles extends StatefulWidget {
 }
 
 class _VehiclesState extends State<Vehicles> {
-  String? email;
+  String email;
+  _VehiclesState({required this.email});
+
   List? vehicles;
   String? registration;
   static String selectedRegistration = '';
@@ -29,61 +34,86 @@ class _VehiclesState extends State<Vehicles> {
     }
   }
 
+  @override
+  void didUpdateWidget(covariant Vehicles oldWidget) {
+    getVehicles();
+    super.didUpdateWidget(oldWidget);
+  }
+
   void editVehicle(registration, brand, model) async {
-    Map arguments = {'email': this.email, 'registration': registration, 'brand': brand, 'model': model};
+    Map arguments = {
+      'email': this.email,
+      'registration': registration,
+      'brand': brand,
+      'model': model
+    };
     Navigator.pushNamed(context, '/editVehicle', arguments: arguments);
   }
 
-  void onChangeSelectedVehicle(index, vehicles){
+  void onChangeSelectedVehicle(index, vehicles) {
     selectedRegistration = vehicles[selectedIndex]['registration'];
-    setState(() => selectedIndex=index);
+    setState(() => selectedIndex = index);
   }
 
   void getVehicles() async {
-    dynamic response = VehiclesSelect(email: this.email).getVehicles();
-    if (response != null && !response['result'].isEmpty) {
-      vehicles = response['result'];
-      selectedRegistration = vehicles![selectedIndex]['registration'];
+    try {
+      vehicles = await VehiclesSelect(email: this.email).getVehicles();
+      buildVehicles(vehicles);
+    } on Exception {
+      print('There was a problem getting vehicles');
+    }
+  }
+
+  void buildVehicles(List? vehicles) {
+    if (vehicles!.length != 0) {
+      selectedRegistration = vehicles[selectedIndex]['registration'];
       setState(() {
         vehiclesWidget = ListView.builder(
           physics: const AlwaysScrollableScrollPhysics(),
-          itemCount: vehicles!.length,
+          itemCount: vehicles.length,
           itemBuilder: (BuildContext ctxt, int index) {
             return InkWell(
               onTap: () {
-                selectedIndex=index;
-                selectedRegistration = vehicles![selectedIndex]['registration'];
-                setState(()=>getVehicles());
+                setState(() {
+                  selectedIndex = index;
+                  buildVehicles(vehicles);
+                });
               },
               child: new Card(
-                shape: (selectedIndex==index)
-                              ? RoundedRectangleBorder(
-                                  side: BorderSide(color: Colors.amber, width: 3.0))
-                              : null,
+                  shape: (selectedIndex == index)
+                      ? RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                          side: BorderSide(color: Colors.amber, width: 3.0))
+                      : RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                        ),
                   elevation: 20,
                   child: ListTile(
-                      tileColor: Colors.blue,
+                      tileColor: Colors.white70,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      ),
                       title: Text(
-                          '${vehicles![index]['registration']}\n${vehicles![index]['brand']} ${vehicles![index]['model']}'),
+                          '${vehicles[index]['registration']}\n${vehicles[index]['brand']} ${vehicles[index]['model']}'),
                       leading: CircleAvatar(
-                        backgroundColor: Colors.white,
+                        backgroundColor: Colors.amber[700],
                         child: Icon(
                           Icons.time_to_leave_rounded,
-                          color: Colors.amber[700],
+                          color: Colors.white,
                         ),
                       ),
                       trailing: RawMaterialButton(
                         onPressed: () {
-                          String registration = vehicles![index]['registration'];
-                          String brand = vehicles![index]['brand'];
-                          String model = vehicles![index]['model'];
+                          String registration = vehicles[index]['registration'];
+                          String brand = vehicles[index]['brand'];
+                          String model = vehicles[index]['model'];
                           editVehicle(registration, brand, model);
                         },
                         elevation: 2.0,
-                        fillColor: Colors.white,
+                        fillColor: Colors.blue[800],
                         child: Icon(
                           Icons.edit,
-                          color: Colors.blue[800],
+                          color: Colors.white,
                         ),
                         padding: EdgeInsets.all(10.0),
                         shape: CircleBorder(),
@@ -94,17 +124,29 @@ class _VehiclesState extends State<Vehicles> {
       });
     } else {
       setState(() {
-        vehiclesWidget = Card(
-          elevation: 20,
-          child: ListTile(
-            leading: Icon(
-              Icons.warning_amber_rounded,
-              color: Colors.amber,
-              size: 30,
+        vehiclesWidget = ListView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      ),
+              elevation: 20,
+              color: Colors.white10,
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                      ),
+                leading: Icon(
+                  Icons.warning_amber_rounded,
+                  color: Colors.amber,
+                  size: 30,
+                ),
+                title: Text('No se han encontrado vehículos',
+                    style: TextStyle(fontSize: 20.0)),
+              ),
             ),
-            title: Text('No se han encontrado vehículos',
-                style: TextStyle(fontSize: 20.0)),
-          ),
+          ],
         );
       });
     }
@@ -112,18 +154,32 @@ class _VehiclesState extends State<Vehicles> {
 
   Future<void> _refreshData() async {
     this.getVehicles();
-    Future.delayed(Duration(seconds: 3));
+    await Future.delayed(Duration(seconds: 3));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getVehicles();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            Navigator.pushNamed(context, '/newVehicle', arguments: email);
-          },
+      floatingActionButton: FloatingActionButton(
+        heroTag: "btn1",
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.pushNamed(context, '/newVehicle', arguments: email);
+        },
+      ),
+      body: SafeArea(
+        child: RefreshIndicator(
+          child: vehiclesWidget,
+          onRefresh: _refreshData,
+          displacement: 40.0,
         ),
-        body: RefreshIndicator(child: vehiclesWidget, onRefresh: _refreshData,));
+      ),
+    );
   }
 }
