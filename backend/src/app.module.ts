29cb0578a4b8437d421c15f66ef7d092347cf259
alvaromerinog <amerino.informatica@gmail.com>
2026-05-09 21:16:53 +0200

    chore(backend): remove default endpoint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule, CarsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
