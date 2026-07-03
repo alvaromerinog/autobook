ff16cfab74fbc2b703a94fa208c10e15ba897138
alvaromerinog <amerino.informatica@gmail.com>
2026-07-03 18:42:58 +0200

    chore(cars): remove duplicated decorators

--- backend/src/cars/api/dto/car.dto.ts
+++ backend/src/cars/api/dto/car.dto.ts
@@ -8,22 +8,16 @@ export class CarDto {
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
   id: string;
 
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
   brand: string;
 
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
   model: string;
 
   @IsInt()
@@ -31,23 +25,16 @@ export class CarDto {
   @Min(1886)
   @MaxModelYear()
   @ApiProperty()
-  @IsInt()
-  @Min(1886)
-  @MaxModelYear()
   year: number;
 
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
   licensePlate: string;
 
   @IsOptional()
   @IsString()
   @ApiProperty({ type: String, nullable: true })
-  @IsOptional()
-  @IsString()
   color: string | null;
 
   @IsOptional()
@@ -55,9 +42,6 @@ export class CarDto {
   @Type(() => Number)
   @Min(0)
   @ApiProperty({ type: Number, nullable: true })
-  @IsOptional()
-  @IsInt()
-  @Min(0)
   mileage: number | null;
 
   constructor(car?: Car) {
